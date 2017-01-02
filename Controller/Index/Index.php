a22a3113da7bef19585c8be3aeca3cb7acd13e87
mss <ymss.deepakmankotia@gmail.com>
2017-01-02 17:28:27 +0530

    Issue Fixed

--- Controller/Index/Index.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Magegeeks\Hidepriceandcartbutton\Controller\Index;
-class Index extends \Magento\Framework\App\Action\Action
-{
-    public function execute()
-    {
-        echo 'Welcome to my extension';
-    }
-}
