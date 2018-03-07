db61f38fc1a795c1c1fc86240f5fdef214a8147a
Deepak Mankotia <mss.deepakmankotia@gmail.com>
2018-03-07 16:35:56 +0530

    Indentation updated

--- Helper/Data.php
+++ Helper/Data.php
@@ -3,12 +3,23 @@ namespace Magegeeks\Hidepriceandcartbutton\Helper;
 
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
+    protected $_session;
+    protected $_scopeConfig;
+
+    public function __construct(
+        \Magento\Customer\Model\Session $session,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+    ) {
+        $this->_session = $session;
+        $this->_scopeConfig = $scopeConfig;
+    }
+
     /*
     * @return string
     */
     public function getConfig($config_path)
     {
-        return $this->scopeConfig->getValue(
+        return $this->_scopeConfig->getValue(
             $config_path,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
@@ -19,8 +30,6 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     */
     public function isLoggedIn()
     {
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $context = $objectManager->get('Magento\Framework\App\Http\Context');
-        return $context->getValue(\Magento\Customer\Model\Context::CONTEXT_AUTH);
+        return $this->_session->isLoggedIn();
     }
 }
